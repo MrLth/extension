1ee97b24a95c4db2b2602d58e607f39fa557f239
mrlth <mrlth@qq.com>
2021-02-19 15:16:20 +0800

    upd webpack config: thread-loader to parse typescript with ts-loader

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -3,27 +3,32 @@
  * @Author: mrlthf11
  * @LastEditors: mrlthf11
  * @Date: 2020-05-27 15:30:26
- * @LastEditTime: 2021-02-18 17:04:15
+ * @LastEditTime: 2021-02-19 15:13:07
  * @Description: file content
  */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HappyPack = require('happypack')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-// 解析命令行参数
+// 解析命令行参数, 但是配合 webpack-cli 会报错
 // const argv = require('minimist')(process.argv.slice(2))
+
+// const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+// const smp = new SpeedMeasurePlugin();
 
 const entry = {
     newtab: path.resolve('src/pages/newtab/index.tsx'),
     popup: path.resolve('src/pages/popup/index.tsx'),
 }
 
-
 console.log('entry', entry)
 
-module.exports = {
+module.exports = /*smp.wrap(*/{
     mode: "production",
     entry,
     output: {
@@ -31,12 +36,7 @@ module.exports = {
         filename: '[name].[contenthash:8].bundle.js',
         publicPath: '/'
     },
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "inline-cheap-source-map",
-    // devtool: "inline-source-map",
-
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js"],
         alias: {
             "@img": path.resolve("public/img"),
@@ -51,21 +51,19 @@ module.exports = {
     optimization: {
         minimize: false,
         splitChunks: {
-            // 对所有的包进行拆分
             chunks: 'all',
+            // 被多次引入的包会优先分出来，不受 maxInitialRequests 限制
             maxInitialRequests: 4,
-            // minSize: 0,
             cacheGroups: {
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
                     name(module) {
-                        // 获取第三方包名
                         const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
                         // npm 软件包名称是 URL 安全的，但是某些服务器不喜欢@符号
                         return `npm.${packageName.replace('@', '')}`;
                     },
                 },
-            },
+            }
         },
     },
     module: {
@@ -75,27 +73,37 @@ module.exports = {
                 exclude: /node_modules/,
                 use: [
                     {
-                        loader: "ts-loader"
+                        loader: 'thread-loader',
+                        options: {
+                            // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+                            workers: require('os').cpus().length - 1,
+                            poolTimeout: Infinity // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
+                        },
+                    },
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+                        }
                     }
                 ]
-            },
-            //  All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
             },
             {
                 test: /\.s?css$/,
                 use: [
+                    //#region style-loader 与 MiniCssExtractPlugin 无法同时使用,并且 MiniCssExtractPlugin 无法用于 HMR ，最好只用于生产环境
+                    /** *
                     {
                         loader: 'style-loader'
-                    }, {
+                    },
+                    /** */
+                    //#endregion
+                    MiniCssExtractPlugin.loader,
+                    {
                         loader: 'css-loader',
                         options: {
                             modules: {
-                                auto: /\.(local|module|m)\.s?css$/,
-                                localIdentName: '[folder]__[local]__[hash:base64:5]',
+                                auto: /\.(local|module|m)\.s?css$/
                             }
                         }
                     },
@@ -118,7 +126,6 @@ module.exports = {
                 type: 'asset',
                 // 现在，webpack 将按照默认条件，自动地在 resource 和 inline 之间进行选择：
                 // 小于 8kb 的文件，将会视为 inline 模块类型，否则会被视为 resource 模块类型。
-                // 自定义设置
                 parser: {
                     dataUrlCondition: {
                         maxSize: 8 * 1024
@@ -129,8 +136,8 @@ module.exports = {
     },
 
     plugins: [
-        new CleanWebpackPlugin(),
-
+        new (require("clean-webpack-plugin").CleanWebpackPlugin),
+        //#region HTML模板
         new HtmlWebpackPlugin({
             inject: true,
             chunks: ['newtab'],
@@ -145,6 +152,7 @@ module.exports = {
                 template: './template/newtab.html'
             })]
             : [],
+        //#endregion
         new CopyWebpackPlugin({
             patterns: [
                 { from: './config/manifest.json' },
@@ -152,15 +160,27 @@ module.exports = {
                 { from: './public/asset/react.production.min.js' }
             ]
         }),
-        //多线程压缩
+        // CSS 抽离
+        new MiniCssExtractPlugin(),
+        // CSS 压缩
+        new CssMinimizerPlugin(),
+        // 多线程压缩
         new ParallelUglifyPlugin({
-            exclude:/min.js$/,
+            exclude: /min.js$/,
             terser: true,
         }),
-        // new (require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+        new ForkTsCheckerWebpackPlugin({
+            typescript: {
+                diagnosticOptions: {
+                    semantic: true,
+                    syntactic: true,
+                },
+            },
+        }),
+        new (require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
     ],
     externals: {
         react: "React",
         "react-dom": "ReactDOM"
     }
-};
+}//)
